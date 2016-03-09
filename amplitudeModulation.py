2f6ef8f8387b5444b8a1f739c328a4ed7d98bb3c
ryan barnes <rbarnes@chem.ucsb.edu>
2016-03-09 13:32:16 -0800

    add function to calculate and compensate for phase and amplitude modulations

--- amplitudeModulation.py
+++ amplitudeModulation.py
@@ -4,6 +4,7 @@ This is to correct for the non linearity of the multiplier circuits via interpol
 
 """
 import matlablike as pys
+import synthesize as s
 close('all')
 
 def pullCsvData(fileName,numberOfCols,popHeader=True,delimiter = '\n'):#{{{
@@ -21,24 +22,43 @@ def pullCsvData(fileName,numberOfCols,popHeader=True,delimiter = '\n'):#{{{
             dataArray[count,column] = float(line[column])
     return dataArray#}}}
 
+try:
+	p
+except:
+	p = s.pulsegen()
+close('all')
 
 outputDataFile = 'outputAmplitude.csv'
+pys.ion()
 
 # constants
 dynamicRangeMin = 0.3 # This is where the modulation starts.
-dynamicRangeMax = 0.4
+dynamicRangeMax = 0.41
 freqOffset = 10e6 #Hz
-freqWidth = 30e6 # Hz, I run plus and minus this width. This is also the width at 200 GHz, I scale approprately in code.
-chirpLength = 1e-6 # seconds
+freqWidth = 10e6 # Hz, I run plus and minus this width. This is also the width at 200 GHz, I scale approprately in code.
+chirpLength = 10e-6 # seconds
 amplitudeScalingFactor = 0.37
+
+wave = p.make_highres_waveform([('rect',0,chirpLength+1e-6)],resolution = 1e-9)
 
 
 outputData = pullCsvData(outputDataFile,3)
 outputData = pys.nddata(outputData[:,0]+1j*outputData[:,1]).rename('value','digAmp').labels('digAmp',outputData[:,2])
 # take only what we can use and set to full scale
-outputClean = outputData['digAmp',lambda x: logical_and(x>dynamicRangeMin,x<dynamicRangeMax)]
-outputClean -= outputClean.data.min()
-outputClean /= outputClean.data.max()
+outputClean = outputData['digAmp',lambda x: logical_and(x>=dynamicRangeMin,x<=dynamicRangeMax)]
+# Clean up the amplitude
+outputAmp = outputClean.runcopy(abs)
+outputAmp.data -= outputAmp.data.min()
+outputAmp /= outputAmp.data.max()
+pys.figure()
+pys.plot(outputData.runcopy(abs))
+pys.plot(outputAmp.runcopy(abs))
+# calculate the phase roll at given amp, unwrap the roll so we can interpolate.
+outputClean.data = arctan2(outputClean.runcopy(imag).data,outputClean.runcopy(real).data)
+outputClean.data = unwrap(outputClean.data)
+pys.figure()
+pys.plot(outputClean,'r.')
+pys.title('phase roll')
 
 timeAxis = r_[0:chirpLength:1e-9]
 freqWidth /= 16.
@@ -51,9 +71,29 @@ planeWave = pys.nddata(exp(1j*2*pi*freqOffset*timeAxis)).rename('value','t').lab
 
 # amplitude modulation
 sinMod = pys.nddata(sin(pi/chirpLength*timeAxis)).rename('value','t').labels(['t'],[timeAxis])
+# the corrected amplitude
+correctedDigAmp = nddata(interp(sinMod.data,outputAmp.data,outputAmp.getaxis('digAmp'))).rename('value','t').labels('t',timeAxis)
+# the phase roll for our amplitude input
+phaseRoll = nddata(interp(correctedDigAmp.data,outputClean.getaxis('digAmp'),outputClean.data)).rename('value','t').labels('t',timeAxis)
+pys.figure()
+pys.plot(phaseRoll)
+pys.title('phase roll for amplitude input')
+
+# now correct the phase of the chirp pulse
+correctedChirp = chirp.copy()
+correctedChirp.data = chirp.data*exp(1j*phaseRoll.data)
+
 # plot the chirp before frequency offset
-pys.ion()
+pys.figure()
 pys.plot(sinMod)
 pys.plot(chirp)
+pys.plot(correctedDigAmp)
+pys.plot(correctedChirp)
+
+toSynthesize = correctedChirp*correctedDigAmp
+pys.figure()
+pys.plot(toSynthesize)
+
+wave['t',0:len(toSynthesize.data)] = toSynthesize.data
 
 pys.show()
